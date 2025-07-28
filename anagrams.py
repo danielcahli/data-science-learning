dbc2460a356f64f9fbdb0552064489f1c5df92bc
DANIEL MONNERAT CAHLI <danielcahli@gmail.com>
2025-07-28 18:24:42 -0300

    Delete anagrams.py

--- anagrams.py
+++
@@ -1,9 +0,0 @@
-word_1 = input('Write the first text: ').replace(" ", "").lower()
-word_2 = input('Write the second text: ').replace(" ", "").lower()
-
-if sorted(word_1) == sorted(word_2):
-    print("Anagrams")
-else:
-    print("Not anagrams")    
-
-         
